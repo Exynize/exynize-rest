e8fc71ef717183ddb138f42315b7565dc6e555a2
Tim Ermilov <yamalight@gmail.com>
2015-12-15 13:14:37 +0100

    allow using env vars for configuration

---
+++ config.js
@@ -0,0 +1,23 @@
+export const auth = {
+    salt: process.env.EXYNIZE_AUTH_SALT || 'default-auth-salt',
+};
+export const db = {
+    host: process.env.EXYNIZE_DB_HOST || 'docker.dev',
+    database: process.env.EXYNIZE_DB_NAME || 'exynizedb',
+    user: '',
+    password: '',
+};
+
+export const email = {
+    host: process.env.EXYNIZE_MAIL_HOST || 'mail.server.com',
+    port: process.env.EXYNIZE_MAIL_PORT || 465,
+    secure: process.env.EXYNIZE_MAIL_SECURE ? true : false,
+    auth: {
+        user: process.env.EXYNIZE_MAIL_USER || 'user@server.com',
+        pass: process.env.EXYNIZE_MAIL_PASS || 'password'
+    },
+};
+
+export const jwtconf = {
+    secret: process.env.EXYNIZE_JWT_SECRET || 'default-jwt-secret',
+};
--- setup.js
+++ setup.js
@@ -1,21 +1,9 @@
 import Promise from 'bluebird';
-import fs from 'fs';
 import {exec} from 'child_process';
 
 // promisify functions
-const asyncAccess = Promise.promisify(fs.access);
 const asyncExec = Promise.promisify(exec);
 
-// copy config from template if needed
-const setupConfig = async () => {
-    try {
-        await asyncAccess('config.js');
-        console.log('[setup]: config exists!');
-    } catch (err) {
-        console.log('[setup]: no config, copying from example...');
-        fs.createReadStream('config.js.example').pipe(fs.createWriteStream('config.js'));
-    }
-};
 // installs node modules for client components
 const setupClientPackages = async () => {
     await asyncExec('cd src/client/ && npm install');
@@ -29,10 +17,13 @@ const setupVmPackages = async () => {
 
 // execute all setup steps
 const setup = async () => {
-    console.log('[setup]: generating config and installing packages for components...');
-    await* [setupConfig(), setupClientPackages(), setupVmPackages()];
+    console.log('[setup]: installing packages for components...');
+    await* [setupClientPackages(), setupVmPackages()];
 };
 
 // start setup
 setup()
-.catch(e => console.error('error during setup:', e));
+.catch(e => {
+    console.error('error during setup:', e);
+    process.exit(1);
+});
