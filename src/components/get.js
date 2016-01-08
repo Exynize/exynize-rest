44089693cf85c4315bf7a86a8a173be0742fb8f2
Tim Ermilov <yamalight@gmail.com>
2016-01-08 14:07:54 +0100

    respect private source flag

--- src/components/get.js
+++ src/components/get.js
@@ -6,9 +6,24 @@ import {asyncRequest} from '../util';
 const handler = async (req, res) => {
     logger.debug('searching for components');
     // find components
-    const components = await Component.find(
+    const result = await Component.find(
         r.row('isPublic').eq(true).or(r.row('user').eq(req.userInfo.id))
     );
+    // remove source where needed
+    const components = result.map(component => {
+        // always show full info to creator
+        if (component.user.id === req.userInfo.id) {
+            return component;
+        }
+        // show source if flag is set
+        if (component.isSourcePublic) {
+            return component;
+        }
+        // otherwise - delete source and return
+        delete component.source;
+        return component;
+    });
+    // return
     res.status(200).json(components);
 };
 
