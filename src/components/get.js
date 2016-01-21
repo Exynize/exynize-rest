d28cfbf6cf92bd76e7e8849e8dd6e2e20a43099b
Tim Ermilov <yamalight@gmail.com>
2016-01-21 12:28:13 +0100

    remove private source from full component info closes #16

--- src/components/get.js
+++ src/components/get.js
@@ -6,6 +6,11 @@ const handler = async (req, res) => {
     logger.debug('searching for component', req.params.user, req.params.component);
     // find component
     const component = await Component.getByUserAndRef(req.params.user, req.params.component);
+    // delete source if flag is set and user is not owner
+    if (!component.isSourcePublic && component.user.id !== req.userInfo.id) {
+        // otherwise - delete source and return
+        delete component.source;
+    }
     // return
     res.status(200).json(component);
 };
