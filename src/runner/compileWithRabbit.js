9582b16217de6eb398f85bf7451bb22249fa03cf
Tim Ermilov <yamalight@gmail.com>
2016-01-27 14:37:07 +0100

    correctly cleanup after compilation using rabbit

--- src/runner/compileWithRabbit.js
+++ src/runner/compileWithRabbit.js
@@ -5,6 +5,7 @@ import {getChannel} from './connection';
 export const compileWithRabbit = (id, source) => new Promise((resolve, reject) => {
     logger.debug('compiling');
     const run = async () => {
+        let cleanup = () => {};
         logger.debug('[cwr]: run');
         const channel = await getChannel();
         logger.debug('[cwr]: got channel');
@@ -14,7 +15,7 @@ export const compileWithRabbit = (id, source) => new Promise((resolve, reject) =
         // bind to key
         await channel.bindQueue(queue, rabbit.exchange, 'runner.compileResult.' + id);
         // listen for messages
-        channel.consume(queue, async (incData) => {
+        const {consumerTag} = await channel.consume(queue, async (incData) => {
             const msg = JSON.parse(incData.content.toString());
             logger.debug('[cwr]: got message.');
             // acknowledge
@@ -22,15 +23,23 @@ export const compileWithRabbit = (id, source) => new Promise((resolve, reject) =
             // reject if error
             if (msg.error) {
                 logger.error('[cwr]: error, ', msg.error);
+                cleanup();
                 return reject(msg.error);
             }
             // return data
             logger.debug('[cwr]: done...');
+            cleanup();
             return resolve(msg.data);
         });
         // send
         logger.debug('[cwr]: sending:', id, source);
         channel.publish(rabbit.exchange, 'runner.compile', new Buffer(JSON.stringify({id, source})));
+        // define cleanup
+        cleanup = async () => {
+            logger.debug('[cwr]: cleanup...');
+            await channel.cancel(consumerTag);
+            await channel.unbindQueue(queue, rabbit.exchange, 'runner.compileResult.' + id);
+        };
     };
     // trigger
     run();
