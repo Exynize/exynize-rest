3a88b322acc03d2526baee4ea4359478d009e164
Tim Ermilov <yamalight@gmail.com>
2016-02-22 15:32:50 +0100

    change how private pipelines behave, closes #26

--- src/pipes/get.js
+++ src/pipes/get.js
@@ -4,6 +4,10 @@ import {asyncRequest} from '../util';
 
 const handler = async (req, res) => {
     logger.debug('searching for pipeline', req.params.user, req.params.pipeline);
+    // only allow getting owned pipelines
+    if (req.params.user !== req.userInfo.username) {
+        return res.status(403).json(`You don't have permission to do that!`);
+    }
     // find pipeline
     const pipeline = await Pipeline.getByUserAndRef(req.params.user, req.params.pipeline);
     // return
--- src/pipes/getAll.js
+++ src/pipes/getAll.js
@@ -1,7 +1,17 @@
 import r from 'rethinkdb';
+import _ from 'lodash';
 import logger from '../logger';
 import {Pipeline} from '../db';
 import {asyncRequest} from '../util';
+
+const toPrivate = (c) => _.omit(
+    c.isPublic ? c : {
+        name: 'Private component',
+        description: 'This component is private.',
+        user: {username: 'private'},
+    },
+    ['args', 'params']
+);
 
 const handler = async (req, res) => {
     logger.debug('searching for pipelines');
@@ -9,7 +19,18 @@ const handler = async (req, res) => {
     const pipelines = await Pipeline.find(
         r.row('isPublic').eq(true).or(r.row('user').eq(req.userInfo.id))
     );
-    res.status(200).json(pipelines);
+    // filter out private components
+    const filteredPipelines = pipelines.map(p => {
+        const {components, render, source} = p;
+        return {
+            ...p,
+            components: components.map(toPrivate),
+            render: toPrivate(render),
+            source: toPrivate(source),
+        };
+    });
+    // return
+    res.status(200).json(filteredPipelines);
 };
 
 export default asyncRequest.bind(null, handler);
