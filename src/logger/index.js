d0ac4e2365835622ddf3b06c7748e19410cbb493
Tim Ermilov <yamalight@gmail.com>
2016-03-14 15:33:35 +0100

    update to microwork 0.8 and use better logging for production

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,14 +1,16 @@
 import winston from 'winston';
 
+export const consoleTransport = new winston.transports.Console({
+    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    prettyPrint: process.env.NODE_ENV !== 'production',
+    colorize: process.env.NODE_ENV !== 'production',
+    timestamp: process.env.NODE_ENV !== 'production',
+    logstash: process.env.NODE_ENV === 'production',
+    label: 'rest-api',
+});
+
 const logger = new winston.Logger({
-    transports: [
-        new winston.transports.Console({
-            level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-            colorize: true,
-            timestamp: true,
-            label: 'rest-api',
-        }),
-    ],
+    transports: [consoleTransport],
 });
 
 export default logger;
--- src/runner/service.js
+++ src/runner/service.js
@@ -1,6 +1,7 @@
 import Microwork from 'microwork';
 import {rabbit} from '../../config';
+import {consoleTransport} from '../logger';
 
-const service = new Microwork({host: rabbit.host, exchange: rabbit.exchange});
+const service = new Microwork({host: rabbit.host, exchange: rabbit.exchange, loggingTransports: [consoleTransport]});
 
 export default service;
